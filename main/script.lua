da5841bcf27b9b6e8758afcd9722a6fcac794367
nightguest151 <74433811+Leaor@users.noreply.github.com>
2022-01-04 13:33:04 -0500

    Script

---
+++ main/script.lua
@@ -0,0 +1,9 @@
+local myName = 'NingaEgg'
+if myName == 'NingaEgg' then
+    print('my name us',myName)
+else myName == 'but' then 
+    print('i might smell bad')
+else
+    print('my name is not ningaegg')
+
+end
